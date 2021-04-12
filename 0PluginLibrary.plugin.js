8e5cb16230e7069f2c43d05931dcfab8f4d092aa
sn8ow <80784394+Sn8ow@users.noreply.github.com>
2021-04-12 19:33:30 +0200

    Update 0PluginLibrary.plugin.js

--- 0PluginLibrary.plugin.js
+++ 0PluginLibrary.plugin.js
@@ -137,7 +137,7 @@ module.exports = {
             github_username: "sn8ow",
             twitter_username: "Sn8ow"
         }],
-        version: "1.80.8",
+        version: "1.80.9",
         description: "Gives other plugins utility functions and the ability to emulate v1.",
         github: "https://github.com/Sn8ow/LuaBrary",
         github_raw: "https://raw.githubusercontent.com/Sn8ow/LuaBrary/main/0PluginLibrary.plugin.js"
