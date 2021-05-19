772ca7b85be5a6538f87ce6a61f2e71a7f6eb689
sn8ow <80784394+Sn8ow@users.noreply.github.com>
2021-05-19 08:02:30 +0200

    Update 0PluginLibrary.plugin.js

--- 0PluginLibrary.plugin.js
+++ 0PluginLibrary.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name LuaBrary
- * @version 1.80.27
+ * @version 1.80.28
  * @invite https://discord.gg/TR5XDAAef5
  * @authorLink https://twitter.com/8owSn
  * @donate https://paypal.me/pools/c/8yOv6nZzfh
@@ -137,7 +137,7 @@ module.exports = {
             github_username: "sn8ow",
             twitter_username: "Sn8ow"
         }],
-        version: "1.80.27",
+        version: "1.80.28",
         description: "Gives other plugins utility functions and the ability to emulate v3.",
         github: "https://github.com/Sn8ow/LuaBrary",
         github_raw: "https://raw.githubusercontent.com/Sn8ow/LuaBrary/main/0PluginLibrary.plugin.js"
@@ -147,9 +147,7 @@ module.exports = {
             title: "Internal Changes",
             type: "Add ",
             items: [
-                "Add PayPal donation.",
-		"Fixed Crash Discord.",
-		"Add user Rivioxik As a helper.",
+                "Fixed a bug where it was not possible to add wallpapers.",
 
             ]
         },
